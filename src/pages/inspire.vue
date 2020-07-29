09999bc451054b63f498c66999c528b8d677d529
Yuichiro Smith <contact@yu-smith.com>
2020-07-29 18:54:58 +0900

    add demo page

--- src/pages/inspire.vue
+++ src/pages/inspire.vue
@@ -3,13 +3,27 @@
     <v-flex class="text-center">
       <img src="/v.png" alt="Vuetify.js" class="mb-5" />
       <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
+        &#8220;{{ message }}&#8221;
         <footer>
           <small>
-            <em>&mdash;John Johnson</em>
+            <em>&mdash;{{ author }}</em>
           </small>
         </footer>
       </blockquote>
     </v-flex>
   </v-layout>
 </template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component({
+  asyncData(ctx) {
+    return ctx.$api.randomQuoteQuoteGet().then(response => response.data)
+  },
+})
+export default class Inspire extends Vue {
+  message!: string
+  author!: string
+}
+</script>
