050924b2937427417125de2a6bbbf55fffb39eff
Yuichiro Smith <contact@yu-smith.com>
2020-07-29 18:41:42 +0900

    demo model

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,34 @@
+import random
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
+)
+
+
+class Quote(BaseModel):
+    message: str
+    author: str
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World", "Foo": "bar"}
+
+
+@app.get("/quote", response_model=Quote)
+def random_quote():
+    quotes = [
+        Quote(message="Hello, World", author="Yuichiro Smith"),
+        Quote(message="Good Morning!", author="Mickey Mouse"),
+        Quote(message="WHAT!?", author="Buzz Lightyear"),
+    ]
+    return random.choice(quotes)
 
 
 @app.get("/items/{item_id}")
